4e596736e1c6e5662cb2b298070f37d403330974
Marcio Costa Silva <marcio@hey.com>
2025-10-25 15:55:01 +0700

    Initial Commit

---
+++ Macros/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Macros
+//
+//  Created by Marcio Costa Silva on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Macros/MacrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacrosApp.swift
+//  Macros
+//
+//  Created by Marcio Costa Silva on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MacrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
